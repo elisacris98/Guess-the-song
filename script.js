2bb9b101f771991b8dc57eaeee8f5e53a7e2c85e
elisacris98 <cristina.elisa98@gmail.com>
2023-10-16 14:04:50 -0400

    commit

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+const song = [
+    {title: " 21 guns", artist: "Greeday"},
+    {title: "99 Problems", artist:"Jay-Z"},
+    {title: "A Milli", artist: "Lil Wayne"},
+    {title: "Bye Bye Bye", artist: "NYSYNC"},
+    {title: "Complicated", artist: "Avril Lavinge"},
+    {title: "Crazy in Love", artist:"Beyonce"},
+    {title: "Cry me a river",artist: "Justin Timberlake"},
+    {title: "Hips don't lie", artist:"Shakira"},
+    {title: "Hollaback Girl", artist: "Gwen Stefani"},
+    {title: "In Da Club", artist: "50 cent"},
+    {title: "Let me love you", artist:"Mario"},
+    {title: "Lose yourself", artist: "Eminem"},
+    {title: "Misery Business", artist: "Paramore"},
+    {title: "Ms Jackson", artist: "Outkast"},
+    {title: "Pokerface", artist: "Lady Gaga"},
+    {title: ""}
 
-document.getElementById('submit-guess').addEventListener('click', checkGuess);
 
-const correctSongTitle = "Despacito";
-
-function checkGuess() {
-    const userGuess = document.getElementById('guess-input').value;
-    const resultMessage = document.getElementById('result-message');
-
-    if (userGuess.toLowerCase() === correctSongTitle.toLowerCase()) {
-        resultMessage.textContent = "Correct! You guessed the song!";
-    } else {
-        resultMessage.textContent = "Oops! Try again.";
-    }
-}
+]
